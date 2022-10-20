19e0ab93c9606561b014f0df8c4ed8dbfec01d99
Alex <adahy.to@gmail.com>
2022-10-20 15:51:41 +0200

    sw for gh pages

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,63 @@
-"use strict";
+var APP_PREFIX = "ApplicationName_"; // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = "version_01"; // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION;
+var URLS = [
+  // Add URL you want to cache in this list.
+  "https://github.com/adahyto/bilumusic_premiera/", // If you have separate JS/CSS files,
+  "https://github.com/adahyto/bilumusic_premiera/index.html", // add path to those files here
+];
 
-// Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v1";
+// Respond with cached resources
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
 
-// Add list of files to cache here.
-const FILES_TO_CACHE = ["/offline.html"];
-
-self.addEventListener("install", (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
   );
-
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (evt) => {
-  // Remove previous cached data from disk.
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
+// Cache resources
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(URLS);
+    })
+  );
+});
+
+// Delete outdated caches
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME);
+
       return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
+        keyList.map(function (key, i) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
           }
         })
       );
-    })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (evt) => {
-  // Add fetch event handler here.
-  if (evt.request.mode !== "navigate") {
-    // Not a page navigation, bail.
-    return;
-  }
-  evt.respondWith(
-    fetch(evt.request).catch(() => {
-      return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match("offline.html");
-      });
     })
   );
 });
